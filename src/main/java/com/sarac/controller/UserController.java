6a4d00937b02a470a1675ffdae07d29dbb2813c2
owner <mahirsarac89@gmail.com>
2023-03-08 09:21:12 -0500

    Ticketing Project role added in the drop down

--- src/main/java/com/sarac/controller/UserController.java
+++ src/main/java/com/sarac/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.sarac.controller;
 import com.sarac.dto.UserDTO;
+import com.sarac.service.impl.RoleServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
+    private final RoleServiceImpl roleService;
+
+    public UserController(RoleServiceImpl roleService) {
+        this.roleService = roleService;
+    }
 
     @GetMapping("/create")
     public String createUser( Model model){
 
         model.addAttribute("user",new UserDTO());
-        model.addAttribute("roles");
+        model.addAttribute("roles",roleService.findAll());
         return "user/create";
     }
 }
